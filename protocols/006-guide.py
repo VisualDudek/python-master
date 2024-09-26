15806cd5f2ac80b037a9412b8d0905a4c44c1961
MarcinDudek <dudek.v@gmail.com>
2024-09-26 14:16:34 +0200

    implement method that defines protocol itself

---
+++ protocols/006-guide.py
@@ -0,0 +1,28 @@
+# example of (explicite) formal protocol
+# ellipsis (...) syntax
+# IntAdder and FloatAdder DO NOT inherit from Adder portocol
+from typing import Protocol
+
+
+class Adder(Protocol):
+    def add(
+        self, x, y
+    ): ...  # implements .add() method, which defines the Adder protocol itselft
+
+
+class IntAdder:
+    def add(self, x, y):
+        return x + y
+
+
+class FloatAdder:
+    def add(self, x, y):
+        return x + y
+
+
+def add(adder: Adder) -> None:
+    print(adder.add(2, 3))  # One big change: IDE now support autocopletion
+
+
+add(IntAdder())  # and mypy do not raise issue
+add(FloatAdder())
