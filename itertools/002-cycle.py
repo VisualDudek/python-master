e53f6dd5e2f6472dc85f6372fd69097b4d202db8
MarcinDudek <dudek.v@gmail.com>
2024-09-28 19:05:37 +0200

    itertools cycle

---
+++ itertools/002-cycle.py
@@ -0,0 +1,23 @@
+import itertools
+
+# itertools.cycle(iterable)
+cycler = itertools.cycle(["a", "b", "c"])
+values = [next(cycler) for _ in range(6)]
+assert values == ["a", "b", "c", "a", "b", "c"]
+
+# Roud-Robin Scheduler: Implement a round-robin scheduler that cycles
+# through a list of tasks or participants
+
+# Infinite Progress Indicator:
+import time
+
+
+def progress_indicator():
+    spinner = itertools.cycle(["|", "/", "-", "\\"])
+    while True:
+        print(next(spinner), end="\r")
+        time.sleep(0.2)
+
+
+# Uncomment the following line to see the progress indicator in action
+progress_indicator()
