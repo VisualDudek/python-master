5f0ce3f3b20629db7c5307d30d80aee0e7704342
MarcinDudek <dudek.v@gmail.com>
2024-09-28 18:30:07 +0200

    itertools count

---
+++ itertools/001-count.py
@@ -0,0 +1,29 @@
+import itertools
+
+# _N = TypeVar(
+#    "_N", int, float, SupportsFloat, SupportsInt, SupportsIndex, SupportsComplex
+# )
+
+# iterator that returns evenly spaced values beginning with start.
+counter: itertools.count = itertools.count(start=0, step=1)
+values = [next(counter) for _ in range(5)]
+assert values == [0, 1, 2, 3, 4]
+
+counter: itertools.count = itertools.count(start=10, step=2)
+values = []
+for _ in range(5):
+    values.append(next(counter))
+assert values == [10, 12, 14, 16, 18]
+
+
+# roughly equivalent to generator below
+def count(start: int = 0, step: int = 1):
+    n = start
+    while True:
+        yield n
+        n += step
+
+
+# which using itertools.count looks like this
+for _ in itertools.count(start=0, step=1):
+    ...
