859e79420629fffdc88753e7c3483d55a69a2821
MarcinDudek <dudek.v@gmail.com>
2024-10-28 07:10:04 +0100

    add guide on implementing abstract base classes with custom metaclass

---
+++ metaclasses/012-guide.py
@@ -0,0 +1,69 @@
+# very good example, simple ABC
+# TAKEAWYA: how abc.abstractmethod works under hood, just dunder __isabstractmethod__ attrib.
+# if just prevents to create instance from class that do not implement abstractmethods
+
+
+def abstractmethod(f):
+    f.__isabstract__ = True
+    return f
+
+
+def abstractmethods(cls):
+    seen = set()
+    abstract = []
+    while isinstance(cls, ABCMeta):
+        for key, val in vars(cls).items():
+            if key in seen:
+                continue
+            seen.add(key)
+            if getattr(val, "__isabstract__", False):
+                abstract.append(key)
+        # object is not ABCMeta so mro will have at least 2 entries
+        cls = cls.__mro__[1]
+    return abstract
+
+
+class ABCMeta(type):
+    def __call__(abccls, *args, **kwargs):  # called when you -> A()
+        print("call", abccls, args, kwargs)
+        abstract = abstractmethods(abccls)
+        if abstract:
+            raise TypeError("no implementation for: " + ", ".join(abstract))
+        return super().__call__(*args, **kwargs)
+
+
+class ABC(metaclass=ABCMeta):
+    pass
+
+
+# vvvvvvvv by default you inherit from ABC and mark some function with abstract method
+class A(ABC):
+    def __init__(self, *args, **kwargs):
+        print("init", self, args, kwargs)
+
+    @abstractmethod
+    def f(self):
+        pass
+
+    @abstractmethod
+    def g(self):
+        pass
+
+
+# vvvvvvv here I pretend that I have implementation of abstract method
+class B(A):
+    def f(self):
+        pass
+
+    def g(self):
+        pass
+
+
+# vvvvvvvvvvvvvvv
+try:
+    A()
+except TypeError as e:
+    print(e)
+
+print(f"{type(B())=}")
+print(f"{type(B)=}")
