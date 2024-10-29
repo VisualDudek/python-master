9ce447c75c5af7f7c12cf2489a507ba36e73bc86
MarcinDudek <dudek.v@gmail.com>
2024-10-29 07:50:26 +0100

    add explanation of __init__ and __new__ methods in metaclass guide

--- metaclasses/013-guide.py
+++ metaclasses/013-guide.py
@@ -1,3 +1,18 @@
-# do LAB
-# adding function overloads into Python using a metaclass
-# this is example where metaclasses shine
+# __init__ vs. __new__
+# how those two are called during object construction
+# TAKEAWAY: __new__ returns obj. __init__ do not return anything
+# - __new__ take class and return its instance
+# - __init__ takes ^^^^^^^^^^^^^^^^^^^^^^^^^^^ instance and set/init default values
+
+
+class A:
+    def __new__(cls, *args, **kwargs):
+        print("called new", cls, args, kwargs)
+        return super().__new__(cls)
+
+    def __init__(self, *args, **kwargs):
+        print("called init", self, args, kwargs)
+
+
+# how object construction works
+a = A(1, 2, 3, x=4)
