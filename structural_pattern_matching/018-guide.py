9b136dbc5964f860e8a72b8c4bb2cd492c5a5204
MarcinDudek <dudek.v@gmail.com>
2024-11-18 11:37:49 +0100

    add guide for using capture patterns in structural pattern matching

---
+++ structural_pattern_matching/018-guide.py
@@ -0,0 +1,30 @@
+# Capture patterns are the cornerstone of **destructuting**.
+# You'll often use them as subpatterns to extract piece of information
+# from complex objects that you want to decompose.
+
+
+from dataclasses import dataclass
+from typing import Self
+
+
+@dataclass
+class Command:
+    name: str
+    options: tuple[str, ...]
+    subcommand: None | Self = None
+
+
+command = Command(
+    name="git",
+    options=(),
+    subcommand=Command(
+        name="commit",
+        options=("-a", "-m", "'Initial commit'"),
+    ),
+)
+
+# pyright: reportMatchNotExhaustive=false
+match command:
+    # vvvvvvvvvvvv                 vvvvvvvvvvvvvvvvvvv names can be misleading
+    case Command("git", _, Command(subcommand, options)):
+        print(f"{subcommand=}\n{options=}")
