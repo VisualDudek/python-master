14736e3bf22f7c13b6c2c09a2cad4ea8c55a0934
MarcinDudek <dudek.v@gmail.com>
2024-11-20 10:52:11 +0100

    refactor: enhance structural pattern matching with TypeAlias support

--- structural_pattern_matching/001-guide.py
+++ structural_pattern_matching/001-guide.py
@@ -1,21 +1,27 @@
 # compare following thow block of code with same functionality
 # SPM is more readable and have declarative style
+# TAKEAWAY: TypeAlias
+
+from typing import TypeAlias, List
+
+Number: TypeAlias = int | float
 
 
-def structural_pattern_matching(subject):
+# pyright: reportMatchNotExhaustive=false
+def structural_pattern_matching(subject: List[Number]):
     match subject:
         case list([int() | float() as x, int() | float() as y, 0]):
             print(f"Point({x=}, {y=})")
 
 
-def legacy_code(subject):
-    if isinstance(subject, list) and len(subject) == 3:
+def legacy_code(subject):  # type: ignore
+    if isinstance(subject, list) and len(subject) == 3:  # type: ignore
         if (
             isinstance(subject[0], int | float)
             and isinstance(subject[1], int | float)
             and subject[2] == 0
         ):
-            x, y, _ = subject
+            x, y, _ = subject  # type: ignore
             print(f"Point({x=}, {y=})")
 
 
