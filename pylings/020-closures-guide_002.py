d86d7768373966f19eb1e059a60bf96b01c605fc
MarcinDudek <dudek.v@gmail.com>
2025-03-31 11:03:31 +0200

    pylings: add example for maintaining state with closures using a counter function

---
+++ pylings/020-closures-guide_002.py
@@ -0,0 +1,17 @@
+# Maintaining State With Closures
+
+# Implement counter, this is tricky one
+
+
+def make_counter():
+    pass
+
+
+# --- TESTING ---
+
+counter_a = make_counter()
+assert counter_a() == 1
+assert counter_a() == 2
+
+counter_b = make_counter()
+assert counter_b() == 1
