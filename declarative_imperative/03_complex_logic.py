c55159bc0866e266d6f3127e54efe226a6f4f93a
MarcinDudek <dudek.v@gmail.com>
2025-06-03 13:17:15 +0200

    add: implement data transformation function and main execution logic

---
+++ declarative_imperative/03_complex_logic.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+def transform(data: list[dict[str, Any]]) -> list[str]:
+    result: list[int]= []
+    for item in data:
+        if item["flag"]:
+            try:
+                val = item["a"]["b"].get("c", {}).get("d", 0)
+                result.append(val+5)
+            except KeyError:
+                result.append(-1)
+        else:
+            result.append(-1)
+    return result
+
+
+def main() -> None:
+    data: list[dict[str, Any]] = [
+        {"flag": True, "a": {"b": {"c": {"d": 1}}}},
+        {"flag": False, "a": {"b": {"c": {"d": 2}}}},
+        {"flag": True, "a": {"b": {}}},
+        {"flag": True, "a": {}},
+    ]
+    
+    transformed_data = transform(data)
