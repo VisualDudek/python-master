9376a1357165c6fb57c0fe4852425fffdc43ff98
MarcinDudek <dudek.v@gmail.com>
2025-06-15 10:38:14 +0200

    add: implement SMP instead of isinstance

---
+++ collections/004c-guide.py
@@ -0,0 +1,41 @@
+# overriding magic methods, e.g. add two vectors
+# use @overload decorator to fix type hint AND
+# enable adding scalar to Vector
+# enable postponed evaluation
+# AND
+# use SMP instead of `isinstance`
+
+from __future__ import annotations
+from typing import NamedTuple, overload, Union
+
+
+class Vector(NamedTuple):
+    x: int
+    y: int
+
+    # Only type hints for diff overloads
+    @overload
+    def __add__(self, other: Vector) -> Vector: ...
+
+    @overload
+    def __add__(self, other: int) -> Vector: ...
+
+    # Actual implementation (no @overload decorator)
+    def __add__(self, other: Vector | int) -> Vector: 
+        match other:
+            case Vector(x, y):
+                return Vector(self.x + x, self.y + y)
+            case int():
+                return Vector(self.x + other, self.y + other)
+            case _:
+                return NotImplemented
+
+
+
+# fmt: on
+
+v1 = Vector(1, 2)
+v2 = Vector(3, 4)
+
+print(v1 + v2)  # Vector(x=4, y=6)
+print(v1 + 5)  # Vector(x=6, y=7)
