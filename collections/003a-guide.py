4c3b30bf942383d6db5b80972fc17be28a91fa16
MarcinDudek <dudek.v@gmail.com>
2025-06-15 09:12:34 +0200

    add: implement Point class with distance calculation method

---
+++ collections/003a-guide.py
@@ -0,0 +1,18 @@
+# subclassing NamedTuple and adding methods
+
+from typing import NamedTuple
+
+
+class Point(NamedTuple):
+    x: float
+    y: float
+
+    @property
+    def distance_to_origin(self) -> float:
+        return (self.x**2 + self.y**2) ** 0.5
+
+
+# Usage
+p = Point(3, 4)
+print(p)  # Point(x=3, y=4)
+print(p.distance_to_origin)  # 5.0 - note: no parentheses needed!
