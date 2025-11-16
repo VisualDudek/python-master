b0eabf10be540de01d7b83fd6ac3a54c36409ac1
MarcinDudek <dudek.v@gmail.com>
2025-11-16 07:52:30 +0100

    feat: update memory allocation script to improve clarity and remove unused code

--- pymalloc/004_malloc_stats.py
+++ pymalloc/004_malloc_stats.py
@@ -1,15 +1,12 @@
 # Run with:
 # PYTHONMALLOCSTATS=1 uv run 004_malloc_stats.py
+# will print statistics of pymalloc allocator every time a new
+# pymalloc object arena is created and on shutdown.
+
 
 def allocate_memory():
     # Create various data structures to trigger memory allocations
     large_list = [i for i in range(10)]
-    
-    # Create some strings
-    # strings = [f"string_{i}" * 10 for i in range(10000)]
-    
-    # Nested structures
-    # nested = [[j for j in range(100)] for i in range(1000)]
     
 
 if __name__ == "__main__":
